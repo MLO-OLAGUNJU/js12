772025ba50b2777acd5fd4a66b4d038437a2cac4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-18 16:13:02 +0100

    learning the for loops of javascript

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-// Loops
+/* // Loops
 let myNumber = 0;
 while (myNumber <= 50) {
   console.log(myNumber);
   myNumber++;
   // myNumber += 2;
+}
+
+// don't create an endless loop */
+
+/* //Do while loops
+let myNumber = 0;
+
+do {
+  myNumber += 2;
+  console.log(myNumber);
+} while (myNumber < 50);
+// don't create an endless loop */
+
+/* //for loops
+for (let manny = 0; manny <= 10; manny++) {
+  console.log(manny);
+} */
+
+let myName = "Manny";
+for (let i = 0; i <= array.length; i++) {
+  console.log(myName.charAt(i));
 }
